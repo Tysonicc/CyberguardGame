844916cc6d3fee7e5a56dbd95b20319ab30ffc85
Patryk Nowak <patryk.n02@gmail.com>
2024-12-01 11:49:33 +0100

    Poprawa i uporządkowanie kodu. Usunięcie zbędnych komentarzy.

--- Maze.cs
+++ Maze.cs
@@ -2,25 +2,26 @@
 
 namespace CyberguardGame;
 
-public class Maze{
-    
+public class Maze
+{
     public int[,] Grid {get; private set;}          // Reprezentacja labiryntu
     public int Width {get; private set;}            // Szerokość labiryntu
     public int Height {get; private set;}           // Wysokość labiryntu
-
     public (int X, int Y) EndPoint {get; set;}
 
-    // Konstruktor klasy Labirynt, przyjmujący szerokość i wysokość
-    public Maze(int width, int height){
+    //Konstruktor klasy Labirynt
+    public Maze(int width, int height)
+    {
         Width = width;
         Height = height;
         Grid = new int[height, width];              // Inicjalizacja macierzy
     }
 
     // Metoda generująca labirynt na podstawie macierzy
-    public void GenerateMaze(int[,] matrix, (int X, int Y) endPoint){
-        
-        if (matrix.GetLength(0) != Height || matrix.GetLength(1) != Width){
+    public void GenerateMaze(int[,] matrix, (int X, int Y) endPoint)
+    {
+        if (matrix.GetLength(0) != Height || matrix.GetLength(1) != Width)
+        {
             throw new ArgumentException("Rozmiar macierzy nie zgadza się z wymiarami labiryntu.");
         }
 
@@ -29,33 +30,31 @@ public class Maze{
     }
 
     // Metoda rysująca labirynt
-    public void DrawMaze(Graphics g, int cellWidth, int cellHeight, (int X, int Y) start, (int X, int Y) end){
-        
-        // Kolory charakterystyczne dla Windows XP
+    public void DrawMaze(Graphics g, int cellWidth, int cellHeight, (int X, int Y) start, (int X, int Y) end)
+    {
         Brush wallBrush = new SolidBrush(Color.Black);              // Czarny kolor dla ścian
         Brush emptySpaceBrush = new SolidBrush(Color.White);        // Biały dla pustych przestrzeni
         Brush startBrush = new SolidBrush(Color.Green);             // Zielony dla punktu startowego
         Brush endBrush = new SolidBrush(Color.Red);                 // Czerwony dla punktu końcowego
 
         // Rysowanie labiryntu
-        for (int y = 0; y < Height; y++){
-            
-            for (int x = 0; x < Width; x++){
-                
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {                
                 // Rysowanie ścian (czarne) i pustych przestrzeni (białe)
-                if (Grid[y, x] == 1){                               // Ściana
+                if (Grid[y, x] == 1)                                // Ściana
+                {                                                   
                     g.FillRectangle(wallBrush, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
                 }
-                else{                                               // Pusta przestrzeń
+                else                                                // Pusta przestrzeń
+                {                                                   
                     g.FillRectangle(emptySpaceBrush, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
                 }
             }
         }
 
-        // Rysowanie punktu startowego
-        g.FillRectangle(startBrush, start.X * cellWidth, start.Y * cellHeight, cellWidth, cellHeight);
-
-        // Rysowanie punktu końcowego
-        g.FillRectangle(endBrush, end.X * cellWidth, end.Y * cellHeight, cellWidth, cellHeight);
+        g.FillRectangle(startBrush, start.X * cellWidth, start.Y * cellHeight, cellWidth, cellHeight);      // Rysowanie punktu startowego
+        g.FillRectangle(endBrush, end.X * cellWidth, end.Y * cellHeight, cellWidth, cellHeight);            // Rysowanie punktu końcowego
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,24 +1,22 @@
 ﻿using CyberguardGame;
 
-public class Player{
-    
+public class Player
+{
     public int X {get; private set;}
-    
     public int Y {get; private set;}
-
-    public Player(int x, int y){
-        
+    public Player(int x, int y)
+    {
         X = x;
         Y = y;
     }
 
-    public void Move(Keys key, Maze maze){
-        
+    public void Move(Keys key, Maze maze)
+    {
         int newX = X;
         int newY = Y;
 
-        switch (key){
-            
+        switch (key)
+        {
             case Keys.W:
                 newY--;             // Ruch w górę
                 break;
@@ -34,8 +32,8 @@ public class Player{
         }
 
         // Sprawdzenie granic oraz czy nowa pozycja nie jest ścianą
-        if (newX >= 0 && newX < maze.Width && newY >= 0 && newY < maze.Height && maze.Grid[newY, newX] != 1){
-            
+        if (newX >= 0 && newX < maze.Width && newY >= 0 && newY < maze.Height && maze.Grid[newY, newX] != 1)
+        {
             X = newX;
             Y = newY;
         }
