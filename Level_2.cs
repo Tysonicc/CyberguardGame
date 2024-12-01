ff37ad7af0f64e1364ce5b0e83283349b8fba244
Patryk Nowak <patryk.n02@gmail.com>
2024-12-01 03:32:12 +0100

    Dodanie formularza dla poziomu drugiego i trzeciego.

---
+++ Level_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CyberguardGame
+{
+    public partial class Level_2 : Form
+    {
+        public Level_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Level_3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CyberguardGame
+{
+    public partial class Level_3 : Form
+    {
+        public Level_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
