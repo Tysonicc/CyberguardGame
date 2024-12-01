b09105d2b488370a46073e8221aa3a485f445770
Patryk Nowak <patryk.n02@gmail.com>
2024-12-01 11:49:47 +0100

    Poprawa i uporządkowanie kodu. Usunięcie zbędnych komentarzy. Zmiana macierzy dotyczącej labiryntu.

--- MazeLevel.cs
+++ MazeLevel.cs
@@ -1,19 +1,15 @@
 ﻿namespace CyberguardGame;
-public class MazeLevel{
-    
+public class MazeLevel
+{
     public int[,] Grid {get; private set;}                          // Macierz reprezentująca labirynt
-    
     public (int X, int Y) StartPoint {get; private set;}            // Punkt początkowy
-    
     public (int X, int Y) EndPoint {get; private set;}              // Punkt końcowy
-
     public int Width => Grid.GetLength(1);                          // Liczba kolumn
-    
     public int Height => Grid.GetLength(0);                         // Liczba wierszy
 
     // Konstruktor klasy
-    public MazeLevel(int[,] grid, (int X, int Y) startPoint, (int X, int Y) endPoint){
-        
+    public MazeLevel(int[,] grid, (int X, int Y) startPoint, (int X, int Y) endPoint)
+    {
         Grid = grid;
         StartPoint = startPoint;
         EndPoint = endPoint;
