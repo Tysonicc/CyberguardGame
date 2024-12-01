e7a2ced813623d924919b27d10a04ea0d829ac31
Patryk Nowak <patryk.n02@gmail.com>
2024-12-01 02:24:46 +0100

    Aspekt wyglądowy kodu. Zmiana macierzy dotyczącej generowania labiryntu. Dodanie niezbędnych komentarzy.

--- Maze.cs
+++ Maze.cs
@@ -2,56 +2,51 @@
 
 namespace CyberguardGame;
 
-public class Maze
-{
-    public int[,] Grid { get; private set; } // Reprezentacja labiryntu
-    public int Width { get; private set; }    // Szerokość labiryntu
-    public int Height { get; private set; }   // Wysokość labiryntu
+public class Maze{
+    
+    public int[,] Grid {get; private set;}          // Reprezentacja labiryntu
+    public int Width {get; private set;}            // Szerokość labiryntu
+    public int Height {get; private set;}           // Wysokość labiryntu
 
-    public (int X, int Y) EndPoint { get; set; }
+    public (int X, int Y) EndPoint {get; set;}
 
     // Konstruktor klasy Labirynt, przyjmujący szerokość i wysokość
-    public Maze(int width, int height)
-    {
+    public Maze(int width, int height){
         Width = width;
         Height = height;
-        Grid = new int[height, width]; // Inicjalizacja macierzy
-
+        Grid = new int[height, width];              // Inicjalizacja macierzy
     }
 
     // Metoda generująca labirynt na podstawie macierzy
-    public void GenerateMaze(int[,] matrix, (int X, int Y) endPoint)
-    {
-        if (matrix.GetLength(0) != Height || matrix.GetLength(1) != Width)
-        {
+    public void GenerateMaze(int[,] matrix, (int X, int Y) endPoint){
+        
+        if (matrix.GetLength(0) != Height || matrix.GetLength(1) != Width){
             throw new ArgumentException("Rozmiar macierzy nie zgadza się z wymiarami labiryntu.");
         }
 
-        Grid = matrix; // Ustawienie macierzy labiryntu
-        this.EndPoint = endPoint; // Ustawienie punktu końcowego
+        Grid = matrix;                              // Ustawienie macierzy labiryntu
+        this.EndPoint = endPoint;                   // Ustawienie punktu końcowego
     }
 
-    // Metoda rysująca labirynt w stylu Windows XP
-    public void DrawMaze(Graphics g, int cellWidth, int cellHeight, (int X, int Y) start, (int X, int Y) end)
-    {
+    // Metoda rysująca labirynt
+    public void DrawMaze(Graphics g, int cellWidth, int cellHeight, (int X, int Y) start, (int X, int Y) end){
+        
         // Kolory charakterystyczne dla Windows XP
-        Brush wallBrush = new SolidBrush(Color.Black);    // Czarny kolor dla ścian
-        Brush emptySpaceBrush = new SolidBrush(Color.White); // Biały dla pustych przestrzeni
-        Brush startBrush = new SolidBrush(Color.Green);   // Zielony dla punktu startowego
-        Brush endBrush = new SolidBrush(Color.Red);       // Czerwony dla punktu końcowego
+        Brush wallBrush = new SolidBrush(Color.Black);              // Czarny kolor dla ścian
+        Brush emptySpaceBrush = new SolidBrush(Color.White);        // Biały dla pustych przestrzeni
+        Brush startBrush = new SolidBrush(Color.Green);             // Zielony dla punktu startowego
+        Brush endBrush = new SolidBrush(Color.Red);                 // Czerwony dla punktu końcowego
 
         // Rysowanie labiryntu
-        for (int y = 0; y < Height; y++)
-        {
-            for (int x = 0; x < Width; x++)
-            {
+        for (int y = 0; y < Height; y++){
+            
+            for (int x = 0; x < Width; x++){
+                
                 // Rysowanie ścian (czarne) i pustych przestrzeni (białe)
-                if (Grid[y, x] == 1) // Ściana
-                {
+                if (Grid[y, x] == 1){                               // Ściana
                     g.FillRectangle(wallBrush, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
                 }
-                else // Pusta przestrzeń
-                {
+                else{                                               // Pusta przestrzeń
                     g.FillRectangle(emptySpaceBrush, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
                 }
             }
@@ -60,7 +55,7 @@ public class Maze
         // Rysowanie punktu startowego
         g.FillRectangle(startBrush, start.X * cellWidth, start.Y * cellHeight, cellWidth, cellHeight);
 
-        // Rysowanie punktu końcowego w kolorze czerwonym
+        // Rysowanie punktu końcowego
         g.FillRectangle(endBrush, end.X * cellWidth, end.Y * cellHeight, cellWidth, cellHeight);
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,40 +1,39 @@
 ﻿using CyberguardGame;
 
-public class Player
-{
-    public int X { get; private set; }
-    public int Y { get; private set; }
+public class Player{
+    public int X {get; private set;}
+    public int Y {get; private set;}
 
-    public Player(int x, int y)
-    {
+    public Player(int x, int y){
+        
         X = x;
         Y = y;
     }
 
-    public void Move(Keys key, Maze maze)
-    {
+    public void Move(Keys key, Maze maze){
+        
         int newX = X;
         int newY = Y;
 
-        switch (key)
-        {
+        switch (key){
+            
             case Keys.W:
-                newY--; // Ruch w górę
+                newY--;             // Ruch w górę
                 break;
             case Keys.A:
-                newX--; // Ruch w lewo
+                newX--;             // Ruch w lewo
                 break;
             case Keys.S:
-                newY++; // Ruch w dół
+                newY++;             // Ruch w dół
                 break;
             case Keys.D:
-                newX++; // Ruch w prawo
+                newX++;             // Ruch w prawo
                 break;
         }
 
         // Sprawdzenie granic oraz czy nowa pozycja nie jest ścianą
-        if (newX >= 0 && newX < maze.Width && newY >= 0 && newY < maze.Height && maze.Grid[newY, newX] != 1)
-        {
+        if (newX >= 0 && newX < maze.Width && newY >= 0 && newY < maze.Height && maze.Grid[newY, newX] != 1){
+            
             X = newX;
             Y = newY;
         }
