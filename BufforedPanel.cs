053e7741cd35affe9aefd2a370dcce5a41c2b75e
Patryk Nowak <patryk.n02@gmail.com>
2024-12-01 17:05:18 +0100

    Wprowadzenie dodatkowej klasy w celu rozszerzenia funkcjonalności labiryntu.

---
+++ BufforedPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberguardGame
+{
+    internal class BufforedPanel : Panel
+    {
+        public BufforedPanel()
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+        }
+    }
+}
