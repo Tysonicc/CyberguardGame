e911dd56882d80cfd25ece21f5890dadfb303e06
Patryk Nowak <patryk.n02@gmail.com>
2024-12-27 12:50:52 +0100

    Uporządkowanie i sprawdzenie kodu. Dodanie komentarzy potrzebnych do dokumentacji.

--- MazeLevel.cs
+++ MazeLevel.cs
@@ -1,20 +1,21 @@
 ﻿namespace CyberguardGame;
-public class MazeLevel
+public class MazeLevel                                              // Klasa reprezentująca poziom labiryntu
 {
     public int[,] Grid {get; private set;}                          // Macierz reprezentująca labirynt
     public (int X, int Y) StartPoint {get; private set;}            // Punkt początkowy
     public (int X, int Y) EndPoint {get; private set;}              // Punkt końcowy
-    public int Width => Grid.GetLength(1);                          // Liczba kolumn
-    public int Height => Grid.GetLength(0);                         // Liczba wierszy
+    public int Width => Grid.GetLength(1);                          // Liczba kolumn (szerokość labiryntu)
+    public int Height => Grid.GetLength(0);                         // Liczba wierszy (wysokość labiryntu)
 
     public MazeLevel(int[,] grid, (int X, int Y) startPoint, (int X, int Y) endPoint)
     {
-        Grid = grid;
-        StartPoint = startPoint;
-        EndPoint = endPoint;
+        Grid = grid;                                                // Ustawienie macierzy labiryntu
+        StartPoint = startPoint;                                    // Ustawienie punktu początkowego
+        EndPoint = endPoint;                                        // Ustawienie punktu końcowego
     }
 
-    public static MazeLevel Easy => new MazeLevel(
+    public static MazeLevel Easy => new MazeLevel(                  // Łatwy poziom labiryntu
+        
         new int[,]
             {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -25,11 +26,13 @@ public class MazeLevel
                 { 1, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
                 { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
             },
-        startPoint: (0, 1),
-        endPoint: (9, 5)    
+        
+        startPoint: (0, 1),                                         // Ustawienie punktu początkowego macierzy
+        endPoint: (9, 5)                                            // Ustawienie punktu końcowego macierzy
     );
 
-    public static MazeLevel Medium => new MazeLevel(
+    public static MazeLevel Medium => new MazeLevel(                // Średni poziom labiryntu
+        
         new int[,]
             {
                 { 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
@@ -44,11 +47,13 @@ public class MazeLevel
                 { 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1},
                 { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
             },
-        startPoint: (0, 9),
-        endPoint: (11, 5)
+        
+        startPoint: (0, 9),                                         // Ustawienie punktu początkowego macierzy
+        endPoint: (11, 5)                                           // Ustawienie punktu końcowego macierzy
     );
 
-    public static MazeLevel Hard => new MazeLevel(
+    public static MazeLevel Hard => new MazeLevel(                  // Trudny poziom labiryntu
+        
         new int[,]
             {
                 { 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
@@ -65,7 +70,8 @@ public class MazeLevel
                 { 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             },
-        startPoint: (0, 4),
-        endPoint: (14, 11)
+        
+        startPoint: (0, 4),                                         // Ustawienie punktu początkowego macierzy
+        endPoint: (14, 11)                                          // Ustawienie punktu końcowego macierzy
     );
 }
