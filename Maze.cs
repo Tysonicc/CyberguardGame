1740a13a22d5220a343c43df636c4d3954fcac47
Patryk Nowak <patryk.n02@gmail.com>
2024-12-27 12:57:01 +0100

    Uporządkowanie i sprawdzenie kodu. Dodanie komentarzy potrzebnych do dokumentacji.

--- Maze.cs
+++ Maze.cs
@@ -2,23 +2,24 @@
 
 namespace CyberguardGame;
 
-public class Maze
+public class Maze                                   // Klasa reprezentująca labirynt
 {
-    public int[,] Grid {get; private set;}          // Reprezentacja labiryntu
+    public int[,] Grid {get; private set;}          // Macierz reprezentująca labirynt
     public int Width {get; private set;}            // Szerokość labiryntu
     public int Height {get; private set;}           // Wysokość labiryntu
     public (int X, int Y) EndPoint {get; set;}
 
-    public Maze(int width, int height)
+    public Maze(int width, int height)              // Konstruktor klasy, który inicjalizuje szerokość i wysokość labiryntu
     {
-        Width = width;
-        Height = height;
+        Width = width;                              // Ustawienie szerokości labiryntu
+        Height = height;                            // Ustawienie wysokości labiryntu
         Grid = new int[height, width];              // Inicjalizacja macierzy
     }
 
-    // Generowanie labiryntu na podstawie macierzy
+    // Generowanie labiryntu na podstawie danej macierzy
     public void GenerateMaze(int[,] matrix, (int X, int Y) endPoint)
     {
+        // Sprawdzenie, czy rozmiar macierzy zgadza się z wymiarami labiryntu 
         if (matrix.GetLength(0) != Height || matrix.GetLength(1) != Width)
         {
             throw new ArgumentException("Rozmiar macierzy nie zgadza się z wymiarami labiryntu.");
