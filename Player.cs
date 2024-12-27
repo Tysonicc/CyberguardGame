52ae0504b0671e85c7e58e69e200065a05fd0b2b
Patryk Nowak <patryk.n02@gmail.com>
2024-12-27 13:08:24 +0100

    Uporządkowanie i sprawdzenie kodu. Dodanie komentarzy potrzebnych do dokumentacji.

--- Player.cs
+++ Player.cs
@@ -1,40 +1,48 @@
 ﻿using CyberguardGame;
 
-public class Player
+public class Player                                     // Klasa reprezentująca pozycję gracza
 {
-    public int X {get; private set;}
-    public int Y {get; private set;}
-    public Player(int x, int y)
+    public int X {get; private set;}                    // Właściwość reprezentująca pozycję gracza na osi X
+    public int Y {get; private set;}                    // Właściwość reprezentująca pozycję gracza na osi Y
+    public Player(int x, int y)                         // Konstruktor klasy, który inicjalizuje pozycję gracza
     {
-        X = x;
-        Y = y;
+        X = x;                                          // Ustawienie pozycji X gracza
+        Y = y;                                          // Ustawienie pozycji Y gracza
     }
 
-    public void Move(Keys key, Maze maze)
+    public void Move(Keys key, Maze maze)               // Metoda do poruszania się gracza w labiryncie
     {
-        int newX = X;
-        int newY = Y;
+        int newX = X;                                   // Zmienna potrzebna do przechowania nowej pozycji X gracza
+        int newY = Y;                                   // Zmienna potrzebna do przechowania nowej pozycji Y gracza
 
-        switch (key)
+        switch (key)                                    // Poruszanie się gracza
         {
-            case Keys.W:
+            case Keys.W:                                // Ruch w górę
+                
                 newY--;
                 break;
-            case Keys.A:
+            
+            case Keys.A:                                // Ruch w lewo
+                
                 newX--;
                 break;
-            case Keys.S:
+            
+            case Keys.S:                                // Ruch w dół
+                
                 newY++;
                 break;
-            case Keys.D:
+            
+            case Keys.D:                                // Ruch w prawo
+                
                 newX++;
                 break;
         }
 
+        // Sprawdzenie, czy nowa pozycja gracza jest w obrębie labiryntu i nie jest ścianą
         if (newX >= 0 && newX < maze.Width && newY >= 0 && newY < maze.Height && maze.Grid[newY, newX] != 1)
         {
-            X = newX;
-            Y = newY;
+            X = newX;                                   // Nowa pozycja gracza na osi X
+            Y = newY;                                   // Nowa pozycja gracza na osi Y
         }
     }
 }
